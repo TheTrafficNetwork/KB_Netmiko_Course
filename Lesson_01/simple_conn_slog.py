ef046d657e0f22160aa7bccef2dcf238a8f34cf0
TheTrafficNetwork <32546563+TheTrafficNetwork@users.noreply.github.com>
2021-03-02 22:15:55 -0600

    Update simple_conn_slog.py

--- Lesson_01/simple_conn_slog.py
+++ Lesson_01/simple_conn_slog.py
@@ -1,10 +1,18 @@
 from netmiko import ConnectHandler
 from getpass import getpass
+import time
+
+password = getpass()
+time.sleep(4)
 
 net_connect = ConnectHandler(
     device_type="cisco_ios",
-    host="cisco.lasthop.io",
-    username="pytclass",
-    password=getpass(),
+    host="cisco3.lasthop.io",
+    username="pyclass",
+    password=password,
+    session_log="Lesson_01/cisco3.out",
 )
 print(net_connect.find_prompt())
+output = net_connect.send_command("show ip int brief")
+print(output)
+net_connect.disconnect
