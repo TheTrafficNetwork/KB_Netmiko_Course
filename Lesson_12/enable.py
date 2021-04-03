47f628be5dc823fc8c475a2944f4339f431c5213
TheTrafficNetwork <32546563+TheTrafficNetwork@users.noreply.github.com>
2021-04-02 22:08:00 -0500

    Update enable.py

--- Lesson_12/enable.py
+++ Lesson_12/enable.py
@@ -3,7 +3,11 @@ from getpass import getpass
 from netmiko import ConnectHandler
 
 # Code so automated tests will run properly
-password = os.getenv("NETMIKO_PASSWORD") if os.getenv("NETMIKO_PASSWORD") else getpass()
+password = (
+    os.getenv("NETMIKO_PASSWORD")
+    if os.getenv("NETMIKO_PASSWORD")
+    else getpass()
+)
 
 
 def lower_privileges(net_connect):
